611487b9971d6d34d5345ba1d9d8945c721c831e
rrr1974 <rrr.java@gmail.com>
2020-05-23 18:39:26 +0530

    Angular Project Created for SpringBoot Rest Service

--- onlinebookstore/src/main/java/in/rrr1974/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/in/rrr1974/onlinebookstore/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package in.rrr1974.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import in.rrr1974.onlinebookstore.entity.Book;
 
+@CrossOrigin("*")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
